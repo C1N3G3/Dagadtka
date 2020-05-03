fdf8772d8ceb0d2346337ec87091152575988188
C1N3G3 <g.e.n@gmx.com>
2020-05-03 22:14:33 +0200

    change thumbnails

--- data.js
+++ data.js
@@ -2,46 +2,55 @@ const data = [
     {
         title:'Photo by <a href="https://unsplash.com/@henry_be">Henry Be</a> on <a href="https://unsplash.com/">Unsplash</a>',
         text: '',
-        src: 'images/1.jpg'
+        src: 'images/1.jpg',
+        "src-thumbnail": 'images/1t.jpg'
     },
     {
         title:'Photo by <a href="https://unsplash.com/@szmigieldesign">Lukasz Szmigiel</a> on <a href="https://unsplash.com/">Unsplash</a>',
         text: '',
-        src: 'images/2.jpg'
+        src: 'images/2.jpg',
+        "src-thumbnail": 'images/2t.jpg',
     },
     {
         title:'Photo by <a href="https://unsplash.com/@ideasboom">Qingbao Meng</a> on <a href="https://unsplash.com/">Unsplash</a>',
         text: '',
-        src: 'images/3.jpg'
+        src: 'images/3.jpg',
+        "src-thumbnail": 'images/3t.jpg'
     },
     {
         title:'Photo by <a href="https://unsplash.com/@gerandeklerk">Geran de Klerk</a> on <a href="https://unsplash.com/">Unsplash</a>',
         text: '',
-        src: 'images/4.jpg'
+        src: 'images/4.jpg',
+        "src-thumbnail": 'images/4t.jpg',
     },
     {
         title:'Photo by <a href="https://unsplash.com/@adrihani">Adrihani Rashid</a> on <a href="https://unsplash.com/">Unsplash</a>',
         text: '',
-        src: 'images/5.jpg'
+        src: 'images/5.jpg',
+        "src-thumbnail": 'images/5t.jpg'
     },
     {
         title:'Photo by <a href="https://unsplash.com/@ripato">Ricardo Gomez Angel</a> on <a href="https://unsplash.com/">Unsplash</a>',
         text: '',
-        src: 'images/6.jpg'
+        src: 'images/6.jpg',
+        "src-thumbnail": 'images/6t.jpg'
     },
     {
         title:'Photo by <a href="https://unsplash.com/@jasonortego">Jason Ortego</a> on <a href="https://unsplash.com/">Unsplash</a>',
         text: '',
-        src: 'images/7.jpg'
+        src: 'images/7.jpg',
+        "src-thumbnail": 'images/7t.jpg'
     },
     {
         title:'Photo by <a href="https://unsplash.com/@laursen">Mathias Bach Laursen</a> on <a href="https://unsplash.com/">Unsplash</a>',
         text: '',
-        src: 'images/8.jpg'
+        src: 'images/8.jpg',
+        "src-thumbnail": 'images/8t.jpg'
     },
     {
         title:'Photo by <a href="https://unsplash.com/@timmossholder">Tim Mossholder</a> on <a href="https://unsplash.com/">Unsplash</a>',
         text: '',
-        src: 'images/9.jpg'
+        src: 'images/9.jpg',
+        "src-thumbnail": 'images/9t.jpg'
     },
 ];
